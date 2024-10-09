4f3ce44918d024e04d805382f53d83b841a79b20
Devan Ng <devan.amar.ng@gmail.com>
2024-10-09 15:31:35 -0400

    possible solution 1?

--- src/ub/cse/algo/Solution.java
+++ src/ub/cse/algo/Solution.java
@@ -1,6 +1,10 @@
 package ub.cse.algo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import static ub.cse.algo.Traversals.bfsPaths;
 
 public class Solution {
 
@@ -28,7 +32,13 @@ public class Solution {
      * @return SolutionObject containing the paths, priorities and bandwidths
      */
     public SolutionObject outputPaths() {
-        SolutionObject sol = new SolutionObject();
+
+         HashMap<Integer, Integer> priorities =  new HashMap<Integer, Integer>();
+        for (Client client: clients) {
+            priorities.put(client.id, client.priority);
+        }
+        HashMap<Integer, ArrayList<Integer>> paths = bfsPaths(graph, clients);
+        SolutionObject sol = new SolutionObject(paths, priorities, bandwidths);
         /* TODO: Your solution goes here */
         return sol;
     }
